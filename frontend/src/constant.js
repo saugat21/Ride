b1b2340a6b8e787c9c477b099570166513e8efde
saugat21 <saugatbaral122@gmail.com>
2024-12-16 20:32:01 +0545

    full code

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,8 +1,4 @@
 export const BASE_URL = '';
 
-
-export const USERS_URL = 'http://ride-08cx.onrender.com/api/users';
-export const BOOKINGS_URL = 'http://ride-08cx.onrender.com/api/bookings';
-
-// export const USERS_URL = '/api/users';
-// export const BOOKINGS_URL = '/api/bookings';
+export const USERS_URL = '/api/users';
+export const BOOKINGS_URL = '/api/bookings';
