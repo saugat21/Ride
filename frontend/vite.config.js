3590ac5b45a89998b9ba06a1c204e0a08d5e6a41
saugat21 <saugatbaral122@gmail.com>
2024-12-17 15:27:19 +0545

    test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'http://localhost:5000',
-        target:"https://ride-08cx.onrender.com",
+         target: 'http://localhost:5000',
+        // target:"https://ride-08cx.onrender.com",
         changeOrigin: true,
         secure: false,
       },
