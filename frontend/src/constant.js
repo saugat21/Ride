d028b5066d11784541c70be80a7331f6dcbfd175
saugat21 <saugatbaral122@gmail.com>
2025-01-24 19:33:45 +0545

    saugat

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,4 +1,4 @@
-export const BASE_URL = '';
+export const BASE_URL = 'https://ride-hfrl.onrender.com';
 
-export const USERS_URL = `/api/users`;
-export const BOOKINGS_URL = `/api/bookings`;
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const BOOKINGS_URL = `${BASE_URL}/api/bookings`;
