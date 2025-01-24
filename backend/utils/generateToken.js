1eaf22da56ef54796a116189ab1f0c5c19cab98c
saugat21 <saugatbaral122@gmail.com>
2025-01-24 13:51:55 +0545

    fgf

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) => {
     //set JWT as Http-only cookie
     res.cookie('jwt', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
+        secure:true,
         sameSite: 'None', 
         maxAge: 30 * 24 * 60 * 60 * 1000 //30d
     })
