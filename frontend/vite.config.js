5fc195f47d6efc3f34d584af32dc8d0ec151d818
saugat21 <saugatbaral122@gmail.com>
2025-01-24 14:04:43 +0545

    fgfg

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://ride-hfrl.onrender.com',
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://ride-hfrl.onrender.com',
+  //       changeOrigin: true,
+  //       secure: true,
+  //     },
+  //   },
+  // },
 })
