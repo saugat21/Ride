d928d66b7d2a851f3d85454f981a503312328dbc
saugat21 <saugatbaral122@gmail.com>
2025-02-25 20:32:09 +0545

    updated

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -2,4 +2,3 @@ export const BASE_URL = '';
 
 export const USERS_URL = `/api/users`;
 export const BOOKINGS_URL = `/api/bookings`;
-export const CHAT_URL='/api/chats';
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,14 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './slices/apiSlice';
 import authSliceReducer from './slices/authSlice';
 import bookingSliceReducer from './slices/bookingSlice';
-import chatSliceReducer from './slices/chatSlice'
 
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
         auth: authSliceReducer,
         booking: bookingSliceReducer,
-        chat:chatSliceReducer,
+        
         
     },
     // Adding the api middleware enables caching, invalidation, polling, and other features of RTK Query
