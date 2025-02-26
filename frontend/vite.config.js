bcd5454e089aa29c0a52f6ca76f16804c3caf12f
saugat21 <saugatbaral122@gmail.com>
2025-02-26 20:08:37 +0545

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   server: {
+  //  server: {
   //   proxy: {
   //     '/api': {
   //       target: 'http://localhost:5000',
@@ -12,5 +12,5 @@ export default defineConfig({
   //       secure: true,
   //     },
   //   },
-   },
+  //  },
 })
